5255fba0f72e7cb0f6420afb951868c5d51e331c
yanapeshko <yanapeshko917@gmail.com>
2021-09-22 19:38:00 +0300

    practicing with video

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require('express');
 const exhbs = require('express-handlebars');
 const products = require('./products.json');
 
-const PORT = process.env.PORT || 4444;
-
 const app = express();
 
-app.use(express.static('public'));
 app.set('view engine', 'hbs');
 app.engine(
   'hbs',
@@ -14,31 +11,39 @@ app.engine(
     extname: 'hbs',
   }),
 );
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+  //   console.log('This is callback for app.get(*/*)');
+  //   console.log(req.url);
+  //   res.send({ name: 'mango' });
   res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { cssFileName: 'about', pageTitle: 'О нас' });
+  //   console.log('This is callback for app.get(*/about*)');
+  //   console.log(req.url);
+  //   res.send('<h1>Hello this is /about</h1>');
+  res.render('about', {
+    cssFileName: 'about',
+    pageTitle: 'What you should know about us',
+  });
 });
 
 app.get('/products', (req, res) => {
   res.render('products', {
     products,
     cssFileName: 'products',
-    pageTitle: 'Наши продукты',
+    pageTitle: 'Our products',
   });
 });
-
 app.get('/product/:productId', (req, res) => {
   console.log(req.params);
 
   const product = products.find(p => p.id === req.params.productId);
-
   res.render('product', { product });
 });
-
-app.listen(PORT, () => {
-  console.log(`Application server is running on port ${PORT}`);
+//product/0
+app.listen(4457, () => {
+  console.log(`Application server is running on port ${4457}`);
 });
